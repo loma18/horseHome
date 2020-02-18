f88fe017dfa4bfbf738058eabd8a8c9349b2b537
Loma <909622852@163.com>
2020-02-18 10:19:08 +0800

    修改配置

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   version: "6.12.1",
   note: '订单支付防止重复点击',
-  subDomain: "tz", // https://admin.it120.cc 登录后台首页的专属域名
-  appid: "wxa46b09d413fbcaff", // 您的小程序的appid，购物单功能需要使用
+  subDomain: "loma18", // https://admin.it120.cc 登录后台首页的专属域名
+  appid: "wx26ffee6fee5b83d3", // 您的小程序的appid，购物单功能需要使用
   shareProfile: '百款精品商品，总有一款适合您', // 首页转发的时候话术
   requireBindMobile: true, // 是否强制绑定手机号码才能使用
   kanjiaRequirePlayAd: true // 是否必须要观看视频广告后才可以砍价
